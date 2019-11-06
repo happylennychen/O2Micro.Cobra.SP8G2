6022917d01b9547abcd735f63627f803bad26976
Leon Chen(WH) <leon.chen@o2micro.com>
2019-11-06 10:46:21 +0800

    rename SP8G2 to OZ2608

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("O2Micro.Cobra.SP8G2")]
+[assembly: AssemblyTitle("O2Micro.Cobra.OZ2608")]
 [assembly: AssemblyDescription("OCESP8G2")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("O2Micro")]
-[assembly: AssemblyProduct("O2Micro.Cobra.SP8G2")]
+[assembly: AssemblyProduct("O2Micro.Cobra.OZ2608")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
